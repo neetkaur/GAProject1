c51bb4b3d6e881728453ed697f23d78c3a1923df
Neet <navneetkaur3@gmail.com>
2021-05-30 02:56:46 -0500

    player able to play game once

--- js/script.js
+++ js/script.js
@@ -16,6 +16,25 @@ const colorarray = ['#2b2d2f', '#2678ec', '#7a49a5', '#aaffc3', '#5fe611', '#dcb
 '#82d5e1', '#469990', '#808000', '#f58231', '#c8c6a7', '#962d2d', '#d99879',
 '#cbe8e8', '#ce97b0','#eba83a']
 
+function RGBToHex(rgb) {
+  // Choose correct separator
+  let sep = rgb.indexOf(",") > -1 ? "," : " ";
+  // Turn "rgb(r,g,b)" into [r,g,b]
+  rgb = rgb.substr(4).split(")")[0].split(sep);
+
+  let r = (+rgb[0]).toString(16),
+      g = (+rgb[1]).toString(16),
+      b = (+rgb[2]).toString(16);
+
+  if (r.length == 1)
+    r = "0" + r;
+  if (g.length == 1)
+    g = "0" + g;
+  if (b.length == 1)
+    b = "0" + b;
+
+  return "#" + r + g + b;
+}
 //array for HTML box IDs
 const boxarray = ['box1','box2','box3','box4',
 'box5','box6','box7','box8',
@@ -23,6 +42,9 @@ const boxarray = ['box1','box2','box3','box4',
 'box13','box14','box15','box16']
 let boxcolor=[]
 let box = []
+let done = 0
+let error = 0
+let counter = 0
 for(let i=0 ; i<16 ; i++){
 box[i+1] = document.getElementById(`${boxarray[i]}`)
 //box[i+1].style.backgroundColor = `${boxcolor[i]}`
@@ -92,11 +114,11 @@ function loadgamebox(){
             clearTimeout(timerId);
             heart.classList.add("noshow")
             elem.classList.add("noshow")
-            //timeover();
+            gameover("timeover")
         } else {
             elem.innerHTML = timeLeft
             timeLeft--;
-            selectAndPutColor()
+            //selectAndPutColor()
         }
     }
     // selectColorFromPanel()
@@ -107,15 +129,32 @@ function loadgamebox(){
       colorlist[i].addEventListener("click", function(){
         currentcolor[i] = colorlist[i].style.backgroundColor
         colornow = colorlist[i].style.backgroundColor
+        counter++
        })
      }//forloop ends
     for(let i=0;i<16;i++){
       box[i+1].addEventListener("click",function(){
-        console.log(box[i+1].style.backgroundColor)
-        console.log(boxcolor[i+1])
+        if(boxcolor[i+1] === RGBToHex(colornow) ){
+          done++
+          box[i+1].classList.add("transiton")
+          box[i+1].style.backgroundColor = colornow
+          if(done===16){gameover("16done")}
+                  //sound ting
+        }
+        else{
+          error++
+          if(error === 5){gameover("5error")}
+          //sound tong
+        }
       })
     }
-   }//setputcolorend
+  }//setputcolorends
+  selectAndPutColor()
+  function gameover(evt){
+    if(evt === "timeover"){console.log(evt)}
+    if(evt === "16done"){console.log(evt)}
+    if(evt === "5error"){console.log(evt)}
+  }
  }//gamestarts ends
     // timed for 30 seconds.....
     // click on a box
